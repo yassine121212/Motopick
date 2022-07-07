19558fb522816c7b010308cf20ed893762492d7c
ihsane01 <101754865+ihsane01@users.noreply.github.com>
2022-07-07 16:20:03 +0100

    commit

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -5,9 +5,16 @@ import Chart from "../../components/chart/Chart";
 import List from "../../components/table/Table";
 import { useLocation } from "react-router-dom";
 import { useState,useEffect } from "react";
-import { collection,getDoc,getDocs ,doc} from "firebase/firestore";
+import { collection,getDoc,getDocs ,doc,updateDoc} from "firebase/firestore";
 import { db } from "../../firebase";
 
+import { Link, useNavigate } from "react-router-dom";
+
+
+
+import IconButton from '@mui/material/IconButton';
+import AppBlockingSharpIcon from '@mui/icons-material/AppBlockingSharp';
+import RemoveCircleOutlineSharpIcon from '@mui/icons-material/RemoveCircleOutlineSharp';
  const Single = () => {
   const location = useLocation();
   const [id, setid] = useState(location.state.id);
@@ -23,20 +30,38 @@ import { db } from "../../firebase";
   }
  
 }
+
+const navigate = useNavigate();
+
  
+
+
+
+
+const updateUser=async(id,blackListed)=>{
+  const userDoc=doc(db,"AdminPanelUsers",id);
+  console.log(blackListed)
+    const newFields={blackListed:!blackListed};
+    await updateDoc(userDoc,newFields);
+window.location.reload()
+
+
+  }
+
   useEffect( async () => {
      handluser();
     
-         const orders = collection(db, `users/${id}/orders`)
+         const orders = collection(db, `AdminPanelUsers/${id}/orders`)
         const ordersder = await getDocs(orders)
         console.log(ordersder)
         const workInfo = ordersder.docs.map((doc)=>({
             ...doc.data(), id:doc.id
         }))
         setorders(workInfo);
-    
+    console.table(workInfo)
     
   }, [id]);
+  
   return (
     <div className="single">
       <Sidebar />
@@ -72,11 +97,24 @@ import { db } from "../../firebase";
                   <span className="itemKey">City:</span>
                   <span className="itemValue">{datauser?.city ? datauser?.city : "Inconnu"}</span>
                 </div>
+                <div className="cellAction">
+                {datauser?.blackListed &&(<div
+              className="deleteButton1" 
+              onClick={() => {
+                updateUser(id , datauser?.blackListed);
+              }}
+            >
+              <IconButton color="error" aria-label="add an alarm">
+        <RemoveCircleOutlineSharpIcon />
+      </IconButton> Deblock
+            </div>)}
+               </div>
+               
               </div>
             </div>
           </div>
           <div className="right">
-            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
+            <Chart aspect={3/ 1} title="User Spending ( Last 6 Months)" />
           </div>
         </div>
         <div className="bottom">
